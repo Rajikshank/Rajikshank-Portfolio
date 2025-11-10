cdfb67298b4539fa3d50f38063b64baf352e7e78
Rajikshank <80474719+Rajikshank@users.noreply.github.com>
2025-11-11 00:06:54 +0530

    mobile-fix

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -2,7 +2,7 @@ import { ArrowDownToLine, Github, Linkedin, X } from "lucide-react";
 import { motion } from "motion/react";
 const Social = () => {
   return (
-    <motion.div className=" mt-6 flex  space-x-24">
+    <motion.div className=" mt-6 flex space-x-14 md:space-x-24">
       <motion.div initial={{ scale: 0 }} animate={{ scale: 1 }}>
         <Linkedin className="text-orange-500 bg-gray-700 ring-1 hover:ring-1 hover:cursor-pointer hover:scale-110 hover:ring-white rounded p-0.5  w-6 h-6" />
       </motion.div>
