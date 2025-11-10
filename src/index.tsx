d9fe2dda4bd0e5f79e88addf03d87d37824abb0a
Rajikshank <80474719+Rajikshank@users.noreply.github.com>
2025-11-10 23:11:16 +0530

    update-november-fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
  
 import Header from "./components/Header";
 // import Navbar from "./components/Navbar";
-import AboutMe from "./components/AboutMe";
+//import AboutMe from "./components/AboutMe";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
 import Education from "./components/Education"; 
